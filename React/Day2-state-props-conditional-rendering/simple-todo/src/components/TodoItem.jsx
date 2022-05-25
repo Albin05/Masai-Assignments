ff448b6ce4635da59c4feca135ed54d2b52bd9db
Albin05 <97468317+Albin05@users.noreply.github.com>
2022-05-25 23:08:05 +0530

    assignments

---
+++ React/Day2-state-props-conditional-rendering/simple-todo/src/components/TodoItem.jsx
@@ -0,0 +1,9 @@
+import "./Todo.css";
+
+export const TodoItem = ({ todo, handleStatus }) => {
+  return (
+    <div className="inputbox">
+          {todo.title} - {todo.status ? "Done" : "Not Done"}<input type="radio" onClick={() => handleStatus(todo.id)}/>
+    </div>
+  );
+};
