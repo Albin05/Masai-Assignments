da1444ae2a85a9d56d00b8cb468ecb6fe8de5f23
Albin05 <97468317+Albin05@users.noreply.github.com>
2022-06-05 13:39:21 +0530

    assignment

---
+++ React/Day3-keys-conditional-rendering/simple-todo/src/components/TodoList.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+export const TodoInput = ({ getData }) => {
+  const [text, setText] = useState("");
+
+  return (
+    <div>
+      <input
+        onChange={(e) => {
+          setText(e.target.value);
+        }}
+        type="text"
+        placeholder="Write Something"
+      />
+      <button
+        onClick={() => {
+          getData(text);
+        }}
+      >
+        +
+      </button>
+    </div>
+  );
+};
